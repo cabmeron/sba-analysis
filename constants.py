ca20c738854413d92d34ad75e62a1d17b0775174
cabmeron <116330722+cabmeron@users.noreply.github.com>
2025-05-16 17:15:05 -0700

    update constants

--- constants.py
+++ constants.py
@@ -7,22 +7,22 @@ PROGRAM_STTR = "STTR"
 ALL_PROGRAMS = "Programs"
 
 OUTPUT_FILE_PATH = './out/csv'
-INPUT_FILE_PATH = './award_data_no_abstract.csv'
+INPUT_FILE_PATH = './inp/award_data_no_abstract.csv'
 
 DEPARTMENTS = [
-    "NASA",
-    "NATIONAL SCIENCE FOUNDATION",
-    "NUCLEAR REGULATORY COMMISSION",
-    "ENVIRONMENTAL PROTECTION AGENCY"
-    "DEPARTMENT OF ENERGY",
-    "DEPARTMENT OF DEFENSE",
-    "DEPARTMENT OF COMMERCE",
-    "DEPARTMENT OF EDUCATION",
-    "DEPARTMENT OF AGRICULTURE",
-    "DEPARTMENT OF THE INTERIOR",
-    "DEPARTMENT OF TRANSPORTATION",
-    "DEPARTMENT OF HOMELAND SECURITY",
-    "DEPARTMENT OF HEALTH AND HUMAN SERVICES",
+    "Department of Education",
+    "Department of Commerce",
+    "Department of Energy",
+    "Department of Defense",
+    "Department of Agriculture",
+    "Department of the Interior",
+    "National Science Foundation",
+    "Environmental Protection Agency",
+    "Department of Homeland Security",
+    "Department of Transportation",
+    "Nuclear Regulatory Commission",
+    "Department of Health and Human Services",
+    "National Aeronautics and Space Administration",
 ]
 
 STATES_STATS = {
@@ -95,3 +95,52 @@ US_STATES_LIST = [
     "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
     "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
 ]
+
+AWARD_YEARS = list(range(1983,2026))
+
+AWARD_YEARS_DICT = {
+    1983: 0,
+    1984: 0,
+    1985: 0,
+    1986: 0,
+    1987: 0,
+    1988: 0,
+    1989: 0,
+    1990: 0,
+    1991: 0,
+    1992: 0,
+    1993: 0,
+    1994: 0,
+    1995: 0,
+    1996: 0,
+    1997: 0,
+    1998: 0,
+    1999: 0,
+    2000: 0,
+    2001: 0,
+    2002: 0,
+    2003: 0,
+    2004: 0,
+    2005: 0,
+    2006: 0,
+    2007: 0,
+    2008: 0,
+    2009: 0,
+    2010: 0,
+    2011: 0,
+    2012: 0,
+    2013: 0,
+    2014: 0,
+    2015: 0,
+    2016: 0,
+    2017: 0,
+    2018: 0,
+    2019: 0,
+    2020: 0,
+    2021: 0,
+    2022: 0,
+    2023: 0,
+    2024: 0,
+    2025: 0,
+
+}
